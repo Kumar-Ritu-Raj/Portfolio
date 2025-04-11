089711e82c7a611fcb43a527e0142019efb94ac2
KumarRituRaj <kumarrituraj2000@gmail.com>
2025-04-11 13:50:00 +0530

    fix: ui update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLaptop, faCode } from '@fortawesome/free-solid-svg-icons';
 import { ActiveProps } from './Navbar';
+import '../styles/About.css';
 
 interface Service {
   icon: any;
